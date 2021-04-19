d75e9c2f75c8244d5307a9e1300c4fc7be65960e
unknown <orszaghlev@gmail.com>
2021-04-19 11:35:12 +0200

    Created prompt provider

--- ticket-service-main/src/main/java/com/deik/ticketservice/TicketServiceApplication.java
+++ ticket-service-main/src/main/java/com/deik/ticketservice/TicketServiceApplication.java
@@ -1,23 +1,15 @@
 package com.deik.ticketservice;
 
-import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.shell.jline.PromptProvider;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.deik"})
 public class TicketServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TicketServiceApplication.class, args);
     }
 
-    @Bean
-    public PromptProvider myPromptProvider() {
-        return () -> new AttributedString("Ticket service>",
-                AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
-    }
 }
---
+++ ticket-service-main/src/main/java/com/deik/ticketservice/ui/TicketServicePromptProvider.java
@@ -0,0 +1,16 @@
+package com.deik.ticketservice.ui;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TicketServicePromptProvider implements PromptProvider {
+
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("Ticket service>", AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
+    }
+
+}
